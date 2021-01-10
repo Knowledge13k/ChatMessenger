f40ac2000f49f08d0204d7624a5a49ab3fdc0219
Knowledge13k <knowledgeinkklassified@gmail.com>
2021-01-10 12:19:17 -0600

    log in and registeration setup

---
+++ ChatMessenger/Resources/Extensions.swift
@@ -0,0 +1,37 @@
+//
+//  Extensions.swift
+//  ChatMessenger
+//
+//  Created by TestAdobe on 1/10/21.
+//
+
+import Foundation
+import UIKit
+
+
+extension UIView{
+    
+    public var width: CGFloat{
+        return self.frame.size.width
+    }
+    
+    public var height: CGFloat{
+        return self.frame.size.height
+    }
+    
+    public var top: CGFloat{
+        return self.frame.origin.y
+    }
+    
+    public var bottomn: CGFloat{
+        return self.frame.size.height + self.frame.origin.y
+    }
+    public var left: CGFloat{
+        return self.frame.origin.x
+    }
+    
+    public var rigth: CGFloat{
+        return self.frame.size.width + self.frame.origin.x
+    }
+}
+
