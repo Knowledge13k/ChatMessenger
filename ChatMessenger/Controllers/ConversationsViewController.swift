82bf3224bfbc96ddb6fc229ba3c1af76dfc4b357
Knowledge13k <knowledgeinkklassified@gmail.com>
2021-01-11 22:06:39 -0600

    added log out tab, continue at min 15 of tutural

--- ChatMessenger/Controllers/ConversationsViewController.swift
+++ ChatMessenger/Controllers/ConversationsViewController.swift
@@ -12,7 +12,7 @@ class ConversationsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .red
+      
         
        
     }
