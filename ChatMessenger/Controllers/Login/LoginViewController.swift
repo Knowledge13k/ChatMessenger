1d9b794f3875ac160f66290a81e92d55edce5bf6
Knowledge13k <knowledgeinkklassified@gmail.com>
2021-01-11 20:20:27 -0600

    add authenticaiton, add database

--- ChatMessenger/Controllers/Login/LoginViewController.swift
+++ ChatMessenger/Controllers/Login/LoginViewController.swift
@@ -131,7 +131,10 @@ class LoginViewController: UIViewController {
        
         
         //Firbase log In
-        FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password, completion: {authResult, error in
+        FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password, completion: {[weak self] authResult, error in
+            guard let strongSelf = self else{
+                return
+            }
             
             guard let result = authResult, error == nil else{
                 print("Failed to log in user with email: \(email)")
@@ -139,6 +142,7 @@ class LoginViewController: UIViewController {
             }
             let user = result.user
             print("Logged In User: \(user)")
+            strongSelf.navigationController?.dismiss(animated: true, completion: nil)
         })
         
     }
